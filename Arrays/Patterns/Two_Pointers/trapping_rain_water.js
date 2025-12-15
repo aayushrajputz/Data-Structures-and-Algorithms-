122afa03b20c732e6d60fe2d3a4112d4ef068859
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2025-12-15 23:56:18 +0530

    pattern revison

--- Arrays/Patterns/Two_Pointers/trapping_rain_water.js
+++ Arrays/Patterns/Two_Pointers/trapping_rain_water.js
@@ -15,9 +15,29 @@
  Goal: Implement Approach 2 or 3.
  */
 
-function trap(height) {
-    // Write your magic here
-    // Hint: Water at index i = Min(MaxLeft, MaxRight) - height[i]
+var trap = function (height) {
+    let n = height.length;
+    let leftMax = new Array(n).fill(0)
+    let rightMax = new Array(n).fill(0)
+
+    leftMax[0] = height[0]
+    for (let i = 1; i <= n; i++) {
+        leftMax[i] = Math.max(leftMax[i - 1], height[i])
+    }
+    rightMax[n - 1] = height[n - 1]
+    for (let i = n - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(rightMax[i + 1], height[i])
+    }
+    let totalWater = 0;
+
+    for (let i = 1; i < n; i++) {
+        let water = Math.min(leftMax[i], rightMax[i]) - height[i]
+        if (water > 0) {
+            totalWater = totalWater + water
+        }
+
+    }
+    return totalWater
 }
 
 // Test Case
