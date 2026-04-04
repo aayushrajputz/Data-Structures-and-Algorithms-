911b6af25ce6020d37fca3386346f69608e5a0f2
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2026-04-04 22:01:15 +0530

    BellmanFord algos

--- Striver_SDE_Sheet/Day24_ShortestPaths/bellman_ford.js
+++ Striver_SDE_Sheet/Day24_ShortestPaths/bellman_ford.js
@@ -2,5 +2,22 @@
 // Target: Detect Negative Cycles (Arbitrage/Fraud in systems) 
 
 function bellmanFord(n, edges, src) {
+    let dist = new Array(n).fill(Infinity);
+    dist[src] = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let [u, v, weight] of edges) {
+            if (dist[u] !== Infinity && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+            }
+        }
+    }
+    for (let [u, v, weight] of edges) {
+        if (dist[u] !== Infinity && dist[u] + weight < dist[v]) {
+            return [-1]
+        }
+    }
+
+    return dist;
 
 }                               
