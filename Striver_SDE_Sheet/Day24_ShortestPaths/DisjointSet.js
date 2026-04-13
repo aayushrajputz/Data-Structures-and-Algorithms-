bf5366990d03da96a8674a1e8c3df1175a4a3500
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2026-04-13 10:59:40 +0530

    dsu

--- Striver_SDE_Sheet/Day24_ShortestPaths/DisjointSet.js
+++ Striver_SDE_Sheet/Day24_ShortestPaths/DisjointSet.js
@@ -10,10 +10,35 @@ class DisjointSet {
     }
 
     find(node) {
-        // STEP 1: Implement Find with Path Compression
+        if (this.parent[node] === node) return node;
+        return this.parent[node] = this.find(this.parent[node]);
     }
 
     unionBySize(u, v) {
-        // STEP 2: Implement Union by Size
+        let ulp = this.find(u);
+        let ulu = this.find(v);
+        if (ulp === ulu) {
+            return;
+        }
+        if (this.size[ulp] < this.size[ulu]) {
+            this.parent[ulp] = ulu;
+            this.size[ulu] += this.size[ulp];
+        } else {
+            this.parent[ulu] = ulp;
+            this.size[ulp] += this.size[ulu];
+        }
+        return this.parent;
     }
+
 }
+const ds = new DisjointSet(8);
+ds.unionBySize(1, 2);
+ds.unionBySize(2, 3);
+ds.unionBySize(4, 5);
+ds.unionBySize(6, 7); // (Use n=8 if you want node 7)
+
+console.log("Is 3 and 7 same?", ds.find(3) === ds.find(7) ? "Yes" : "No");
+
+ds.unionBySize(3, 7);
+console.log("After union (3,7), is 3 and 7 same?", ds.find(3) === ds.find(7) ? "Yes" : "No");
+
