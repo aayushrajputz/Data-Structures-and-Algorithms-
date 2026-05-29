32bc5b3f59e17afebcbddd579c59cc8b8a1f814b
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2026-05-29 12:55:36 +0530

    feat: complete Delete Operation for Two Strings and Distinct Subsequences (LeetCode Hard)

---
+++ Striver_SDE_Sheet/Day25_DP/distinct_subsequences.js
@@ -0,0 +1,24 @@
+function distinct_subsequences(s, t) {
+    let n = s.length
+    let m = t.length
+
+    let dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(-1))
+
+    function solve(i, j) {
+        if (j === 0) return 1;
+        if (i === 0) return 0;
+
+        if (dp[i][j] !== -1) return dp[i][j];
+
+        if (s[i - 1] === t[j - 1]) {
+            return dp[i][j] = solve(i - 1, j - 1) + solve(i - 1, j);
+        }
+        else {
+            return dp[i][j] = solve(i - 1, j);
+        }
+
+    }
+    return solve(n, m)
+}
+
+console.log(distinct_subsequences("rabbbit", "rabbit")) 
