d1e307e7c0fe2e4508324e5d2b90cd80cecb2c79
Aayush Rajput <142202623+aayushrajputz@users.noreply.github.com>
2026-06-01 12:46:14 +0530

    DP knapsack

--- Striver_SDE_Sheet/Day25_DP/coin_change.js
+++ Striver_SDE_Sheet/Day25_DP/coin_change.js
@@ -1,30 +1,27 @@
 function coinChange(arr, amt) {
-    let n = arr.length;
-    let dp = Array.from({ length: n }, () => new Array(amt + 1).fill(-1));
+    let n = arr.length
+    let dp = Array.from({ length: n }, () => Array(amt + 1).fill(-1))
 
     function solve(ind, target) {
-        if (target === 0) return 0;
-        
-        // Base Case: 1st coin par aa gaye
-        if (ind === 0) {
-            if (target % arr[0] === 0) return target / arr[0];
-            return Infinity;
-        }
+        if (target === 0) return 0
+        if (ind < 0 || target < 0) return Infinity
 
-        if (dp[ind][target] !== -1) return dp[ind][target];
+        if (dp[ind][target] !== -1) return dp[ind][target]
 
-        let notPick = solve(ind - 1, target);
 
-        let pick = Infinity;
+        let notPick = 0 + solve(ind - 1, target)
+
+        let pick = Infinity
         if (arr[ind] <= target) {
-            pick = 1 + solve(ind, target - arr[ind]);
+            pick = 1 + solve(ind, target - arr[ind])
+            return dp[ind][target] = Math.min(notPick, pick)
         }
 
-        return dp[ind][target] = Math.min(pick, notPick);
+        return dp[ind][target] = notPick
     }
-
-    let ans = solve(n - 1, amt);
-    return ans >= Infinity ? -1 : ans;
+    let ans = solve(n - 1, amt)
+    if (ans >= Infinity) return -1
+    return ans
 }
 
-console.log(coinChange([1, 2, 3], 4)); // Expected: 2 (2 + 2 = 4 or 3 + 1 = 4)
+console.log(coinChange([1, 2, 3], 4)) 
