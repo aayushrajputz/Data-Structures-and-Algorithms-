5b1b2b8b61481a1e58f8a917b8ba6fe611c247bf
aayushrajputz <aayushpratapsingh001@gmail.com>
2026-08-07 17:27:02 +0530

    revsion

--- revison/subsets.js
+++ revison/subsets.js
@@ -9,9 +9,28 @@
  */
 
 function subsets(nums) {
-    // Write your backtracking code here
-    return [];
+    let result = [];
+
+    function backtrack(index, current) {
+        // Base Case: Jab hum saare elements ka decision le lein
+        if (index === nums.length) {
+            result.push([...current]); // Copy push karte hain taaki reference mutation se bachein
+            return;
+        }
+
+        // Option 1: Current element ko INCLUDE karo
+        current.push(nums[index]);
+        backtrack(index + 1, current);
+
+        // Option 2: Current element ko EXCLUDE karo (Backtrack step)
+        current.pop();
+        backtrack(index + 1, current);
+    }
+
+    backtrack(0, []);
+    return result;
 }
 
 // Test case
+console.log("Subsets of [1, 2, 3]:");
 console.log(subsets([1, 2, 3]));
